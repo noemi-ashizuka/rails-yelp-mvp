e67273f286c981e528eb23d086d64302759d6701
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-02-19 20:58:31 +0900

    added restaurants and reviews mcv

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:new, :show, :index, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+
+  
+  # get "/restaurants", to: "restaurants#index", as: :restaurants
+
+  # get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+
+  # get "/restaurant/:id", to: "restaurants#show", as: :restaurant
+
+  # post "restaurants", to: "restaurants#create"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating restaurants..."
+restaurants_attributes = [
+  {
+    name: "LeWagon",
+    address: "Meguro",
+    phone_number: "0009382",
+    category: Restaurant::CATEGORIES.sample
+  },
+  {
+    name: "L'Artista",
+    address: "Nakameguro",
+    phone_number: "8609382",
+    category: Restaurant::CATEGORIES.sample
+  },
+  {
+    name: "Eden",
+    address: "Ebisu",
+    phone_number: "0549384",
+    category: Restaurant::CATEGORIES.sample
+  },
+  {
+    name: "Kitty",
+    address: "Shibuya",
+    phone_number: "0109382",
+    category: Restaurant::CATEGORIES.sample
+  },
+  {
+    name: "Moonlight",
+    address: "Yokohama",
+    phone_number: "0809365",
+    category: Restaurant::CATEGORIES.sample
+  },
+  {
+    name: "Akira",
+    address: "Meguro",
+    phone_number: "0709562",
+    category: Restaurant::CATEGORIES.sample
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+
+puts "done!"
